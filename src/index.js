17a3bd0e4978b25ba8dcedc5e5350bb675fa5cda
benito281 <benitoortiz281@gmail.com>
2020-11-10 22:28:11 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express= require('express');
+//const exphbs=require('express-handlebars');
+const ejs=require('ejs');
+const morgan=require('morgan');
+const multer=require('multer');
+const path=require('path');
+const uuid=require('uuid/v4')
+
+// Innicislizamoss
+const app = express();
+
+//Setting
+app.set('port',process.env.PORT || 3000);
+app.set('views', path.join(__dirname,'views'));
+//app.engine('.hbs', exphbs({extname: '.hbs'}));
+//app.set('view engine', '.hbs');
+app.set("view engine", ".ejs");
+
+//Middlewars
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+const storage= multer.diskStorage({
+  destination:path.join(__dirname, 'public/img/uploads'),
+  filename:(req,file,cb,filename)=>{
+    cb(null,uuid() + path.extname(file.originalname));
+  }
+});
+app.use(multer({
+  storage: storage
+}).single('image'));
+
+
+//Global variables
+
+
+
+//Routes
+app.use(require('./routes/index.routes.js'))
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Star server
+app.listen(app.get('port'),()=>{
+console.log(`Server on port ${app.get('port')}`)
+})
---
+++ src/models/image.js
@@ -0,0 +1,37 @@
+const {Schema,model} = require('mongoose');
+
+const imageSchema= new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    filename:{
+        type:String,
+        required:true
+    },
+    path:{
+        type:String,
+        required:true
+    },
+    originalname:{
+        type:String,
+        required:true
+    },
+    minetype:{
+        type:String
+    },
+    size:{
+        type:Number
+    },
+    created_at_:{
+        type:Date,
+        default:Date.now()
+    }
+    
+});
+
+module.exports= model('Images',imageSchema);
